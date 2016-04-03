086556b7c2d890015513ddce9638ad2a01a26763
alexfrancisross <alexfrancisross@hotmail.com>
2016-04-03 20:55:43 +0100

    upload

---
+++ create wmolocations.sql
@@ -0,0 +1,17 @@
+﻿
+drop table wmolocations;
+CREATE TABLE public.wmolocations
+("block number" INTEGER,
+"station number" INTEGER,
+"icao location" VARCHAR(255),
+"place name" VARCHAR(100),
+"state" VARCHAR(255),
+error VARCHAR(255),
+"country name" VARCHAR(255),
+continent INTEGER,
+longitude float8,
+latitude float8,
+"european region" VARCHAR(255),
+district_level VARCHAR(50),
+postcode VARCHAR(255),
+county VARCHAR(50))
---
+++ create wunderground.sql
@@ -0,0 +1,30 @@
+﻿drop table wunderground;
+
+CREATE TABLE wunderground
+(
+"ID" serial,
+"GMT" timestamp without time zone,
+"Max TemperatureF" character varying(255),
+"Mean TemperatureF" character varying(255),
+"Min TemperatureF" character varying(255),
+"Max Dew PointF" character varying(255),
+"MeanDew PointF" character varying(255),
+"Min DewpointF" character varying(255),
+"Max Humidity" character varying(255),
+"Mean Humidity" character varying(255),
+"Min Humidity" character varying(255),
+"Max Sea Level PressureIn" character varying(255),
+"Mean Sea Level PressureIn" character varying(255),
+"Min Sea Level PressureIn" character varying(255),
+"Max VisibilityMiles" character varying(255),
+"Mean VisibilityMiles" character varying(255),
+"Min VisibilityMiles" character varying(255),
+"Max Wind SpeedMPH" character varying(255),
+"Mean Wind SpeedMPH" character varying(255),
+"Max Gust SpeedMPH" character varying(255),
+"PrecipitationIn" character varying(255),
+"CloudCover" character varying(255),
+"Events" character varying(255),
+"WindDirDegrees" character varying(255),
+"Airport Code" character varying(255)
+)
